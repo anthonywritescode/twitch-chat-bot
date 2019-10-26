54307e600bbbd3949e0f37556a895498403d5633
Anthony Sottile <asottile@umich.edu>
2019-10-26 11:53:29 -0700

    Fix matching at not beginning of string

--- bot.py
+++ bot.py
@@ -320,7 +320,7 @@ def msg_ping(match: Match[str]) -> Response:
 @handle_message(r'.*\b(nano|linux|windows|emacs)\b', flags=re.IGNORECASE)
 def msg_gnu_please(match: Match[str]) -> Response:
     msg, word = match[3], match[4]
-    query = re.match(f'gnu[/+]{word}', msg, flags=re.IGNORECASE)
+    query = re.search(f'gnu[/+]{word}', msg, flags=re.IGNORECASE)
     if query:
         return MessageResponse(match, f'YES! {query[0]}')
     else:
