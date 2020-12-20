8c5f00010672ec3988171f2a9ae5c178421f00cb
Anthony Sottile <asottile@umich.edu>
2020-12-19 17:47:29 -0800

    add !dicsord alias

--- bot/plugins/simple.py
+++ bot/plugins/simple.py
@@ -107,6 +107,7 @@ for _cmd, _msg in _TEXT_COMMANDS:
 
 _ALIASES: Tuple[Tuple[str, Tuple[str, ...]], ...] = (
     ('!bluething', ('!blueball',)),
+    ('!discord', ('!dicsord',)),
     ('!distro', ('!os',)),
     (
         '!editor', (
